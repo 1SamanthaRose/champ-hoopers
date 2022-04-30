07e59e6af0c8c3410f7dbfc26ba699d46cea7d81
agc88 <98765979+agc88@users.noreply.github.com>
2022-04-30 11:42:45 -0400

    Update sim code 6 factor model.R

--- sim code 6 factor model.R
+++ sim code 6 factor model.R
@@ -1,5 +1,5 @@
-#reads the box score data, and subsets it into a data set with
-#only the team names and the six predictors we will be using
+#reads the box score data, and subsets it into a data set with only
+#the team names and the six predictors that we will be using
 box <- read.csv('boxs.csv')
 box6 <- box[,c(1, 35, 34, 15, 32, 20, 21)]
 
@@ -40,23 +40,22 @@ simSeries <- function(teamA, teamB) { #insert teams of choice into here
     AGameWon = 0 #Keeps track of number of games won so far by
     #each team
     BGameWon = 0
-    while ((AGameWon < 4) & (BGameWon < 4)) { #iterates until one 
-    #team has won 4 games 
-  #chooses a reg season game at random for each team from the box score dataset
+    while ((AGameWon < 4) & (BGameWon < 4)) { #iterates until one team has won 4 games 
+        #randomly samples a regular season game for team A
         gameA = teamA[sample(1:nrow(teamA), 1),] 
-        gameB = teamB[sample(1:nrow(teamB), 1),]
-  #the values of important predictors as indicated by the forest, for that game
+        #the values of each predictor from team A's sampled game
         p1A = gameA$efgp
-        p2A = 100 - gameA$tovp 
-    #gives the percentage of possesions NOT resulting in a turnover
+        p2A = 100 - gameA$tovp #gives the percentage of possesions NOT resulting in a turnover
         p3A = gameA$ftp
         p4A = gameA$drebp
         p5A = gameA$stl
         p6A = gameA$blk
         if (p6A == 0) {
-        p6A = 0.0000000000001
-        } #occasionally both teams have zero blocks, so this offsets a division by zero error
-        #in the following section
+        p6A = 0.0000000000001 #occasionally both teams have zero blocks, 
+        #so this offsets a potential division by zero error later on
+        } 
+        #does the same as above for team B
+        gameB = teamB[sample(1:nrow(teamB), 1),]
         p1B = gameB$efgp
         p2B = 100 - gameB$tovp
         p3B = gameB$ftp
@@ -66,26 +65,29 @@ simSeries <- function(teamA, teamB) { #insert teams of choice into here
         if (p6B == 0) {
             p6B = 0.0000000000001
         }
-  #this adds together all of Team A's stats in proportion to the total stats
-  #for both teams, weighing them appropriately. This will always sum to an amount from 0 to 1.
-  #w1-4 are the standardized weights for the respective predictors
-        score = ((p1A / (p1A + p1B)) * w1
-    + (p2A / (p2A + p2B)) * w2
-    + (p3A / (p3A + p3B)) * w3
-    + (p4A / (p4A + p4B)) * w4
-    + (p5A / (p5A + p5B)) * w5
-    + (p6A / (p6A + p6B)) * w6)
-#Team A wins if the value is greater than 0.5, Team B wins if the value is less
-#than 0.5, on the off chance the score is exactly 0.5 just run it again and 
-#that game doesn't count.
+        #For each predictor, this determines the value of that predictor for Team A's in proportion 
+        #to the sum of that value for both teams, weighing that predictor appropriately. Then this
+        #sums all of the weighted proportions. This will always sum to an amount from 0-1.
+        score = (
+          (p1A / (p1A + p1B)) * w1
+        + (p2A / (p2A + p2B)) * w2
+        + (p3A / (p3A + p3B)) * w3
+        + (p4A / (p4A + p4B)) * w4
+        + (p5A / (p5A + p5B)) * w5
+        + (p6A / (p6A + p6B)) * w6
+        )
+        #Team A wins the simulated if score is greater than 0.5, Team B wins the game if score is 
+        #less than 0.5. On the off chance that score is exactly 0.5, nothing is incremented.
         if (score > 0.5) {
             AGameWon = AGameWon + 1
         }
         if (score < 0.5) {
             BGameWon = BGameWon + 1
         }
-  #nested while loop ends here once a team has won four sim games in the series
+    #nested while loop ends here once a team has won four sim games in the series
     }
+    #increments the total of series won for the team that won four games, then returns 
+    #to the first loop, #resetting the games won counters and doing this all again
     if (AGameWon > BGameWon) {
         ASerWon = ASerWon + 1
     }
@@ -93,9 +95,10 @@ simSeries <- function(teamA, teamB) { #insert teams of choice into here
         BSerWon = BSerWon + 1
     }
     seriesCount = seriesCount + 1
-#outer loop ends once 10,000 series have been simulated
+  #outer loop ends once 10,001 series have been simulated
   }
-#this reports the favored team and said team's win probability
+  #this reports the favored team's win probability, based on the proportion of 
+  #series won by that team
   if (ASerWon > BSerWon) {
     winner = teamA
   }
